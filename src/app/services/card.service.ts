1ec808e41ed1b978f81777d72062ae315de996ca
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-08 23:14:22 +0200

    feat: create CardService to manage card data and interact with the server API

---
+++ src/app/services/card.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  private apiUrl = 'http://localhost:3000/cards';
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Fetch all cards from the server
+   * @returns {Observable<any[]>} An observable of card data
+   */
+  getCards(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
