70741440d71fb5fa52245a6489701668f0de8755
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-08 22:58:36 +0200

    feat: update LoginComponent to redirect to Dashboard on login and configure routing with AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from "@angular/core";
-import {LoginComponent} from "./components/auth/login/login.component";
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {AuthGuard} from "./guards/auth.service";
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  login: string = '';
+  password: string = '';
+  error: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  /**
+   * Attempts to log in the user.
+   */
+  onLogin(): void {
+    this.authService.login(this.login, this.password).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigate(['/']);
+      } else {
+        this.error = 'Invalid login or password.';
+      }
+    });
+  }
 }
