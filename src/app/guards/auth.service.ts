cd48b5b559e54a9b33df678588f7e0fb430d4c3b
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-08 22:58:27 +0200

    feat: add DashboardComponent and AuthGuard to protect routes and restrict access for authenticated users

---
+++ src/app/guards/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  /**
+   * Determines if the route can be activated.
+   * If the user is logged in, redirect to the dashboard.
+   * If not, allow access to the route.
+   */
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
+    return true;
+  }
+}
