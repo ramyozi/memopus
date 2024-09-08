161ceb7fff29909be1965102055e544585c4bb4e
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-08 23:01:47 +0200

    refactor: update AppComponent with improved navigation and authentication checks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
+import { AuthService } from './services/auth.service';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'memopus';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  /**
+   * Checks if the user is authenticated.
+   * @returns {boolean} The authentication status.
+   */
+  isAuthenticated(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  /**
+   * Logs out the user and navigates to the login page.
+   */
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
