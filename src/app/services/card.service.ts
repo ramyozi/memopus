20f58411ed2e335c2bbcd0c4072bb9f100a0f026
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-09 00:44:40 +0200

    feat: update CardService to handle dynamic updates and column management for cards

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Card } from '../models/card.model';
 import { Tag } from '../models/tag.model';
+import {Column} from "../models/column.model";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Tag } from '../models/tag.model';
 export class CardService {
   private apiUrl = 'http://localhost:3000/cards';
   private tagsUrl = 'http://localhost:3000/tags';
+  private columnsUrl = 'http://localhost:3000/columns';
 
   constructor(private http: HttpClient) {}
 
@@ -27,5 +29,23 @@ export class CardService {
    */
   getTags(): Observable<Tag[]> {
     return this.http.get<Tag[]>(this.tagsUrl);
+  }
+
+  /**
+   * Update the column of a card
+   * @param {number} cardId - The ID of the card to update
+   * @param {number} columnId - The new column ID for the card
+   * @returns {Observable<Card>} An observable of the updated card
+   */
+  updateCardColumn(cardId: number, columnId: number): Observable<Card> {
+    return this.http.patch<Card>(`${this.apiUrl}/${cardId}`, { column: columnId });
+  }
+
+  /**
+   * Fetch all columns from the server
+   * @returns {Observable<Column[]>} An observable of column data
+   */
+  getColumns(): Observable<Column[]> {
+    return this.http.get<Column[]>(this.columnsUrl);
   }
 }
