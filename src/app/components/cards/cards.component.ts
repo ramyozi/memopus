d8d339d4c1acf09d50845b2edf0ef85b3c6cde84
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-08 23:26:27 +0200

    filters

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { CardService } from '../../services/card.service';
@@ -12,11 +12,18 @@ import { CardService } from '../../services/card.service';
 })
 export class CardsComponent implements OnInit {
   cards: any[] = [];
+  filteredCards: any[] = [];
+
+  @Input() selectedTags: number[] = [];
 
   constructor(private cardService: CardService) {}
 
   ngOnInit(): void {
     this.fetchCards();
+  }
+
+  ngOnChanges(): void {
+    this.filterCards();
   }
 
   /**
@@ -25,6 +32,18 @@ export class CardsComponent implements OnInit {
   fetchCards(): void {
     this.cardService.getCards().subscribe((data) => {
       this.cards = data;
+      this.filterCards();
     });
+  }
+
+  /**
+   * Filter cards based on selected tags
+   */
+  filterCards(): void {
+    if (this.selectedTags.length === 0) {
+      this.filteredCards = this.cards;
+    } else {
+      this.filteredCards = this.cards.filter(card => this.selectedTags.includes(card.tag));
+    }
   }
 }
