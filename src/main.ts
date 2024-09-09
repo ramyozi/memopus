46b19daa2f3a5f94ba8a3095061b386a0fd7b169
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-09 02:52:20 +0200

    Fix "NG05105: Unexpected synthetic property @transitionMessages found" error

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ],
 }).catch(err => console.error(err));
